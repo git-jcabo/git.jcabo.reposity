a320e075ce73a3f93674b726d84d7ca114294935
jcabo-sds <sduoduo_2016@126.com>
2020-08-04 13:26:37 +0800

    first commit

---
+++ mybatis/src/com/jcabo/Jcabo.java
@@ -0,0 +1,15 @@
+package com.jcabo;
+
+public class Jcabo {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main (String[] args) {
+        System.out.println(23);
+        String s = "wew";
+        System.out.println(s);
+    }
+
+}
