6609ed444bbed0316dabbfe1a6bf7bf8ad32b027
jcabo-sds <sduoduo_2016@126.com>
2020-08-04 15:17:39 +0800

    jcabo-spring项目

---
+++ jcabo-spring/src/test/java/org/jcabo/jcabospring/JcaboSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jcabo.jcabospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcaboSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
